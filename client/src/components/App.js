1da4f234d0095fa6673b41032cb2893849e27e6d
Dylan <dmalla@live.com>
2019-10-24 16:54:28 -0700

    Completed layout of client side of request form. Need to make responsive and fix minor cosmetic details. Need to implement hidden scrollbar on Projects in Progress div.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,13 +3,16 @@ import '../styles/main.css'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import store from '../store'
+
 import DesignRequest from './DesignRequest'
+import Header from './Header'
 
 export default props => {
 
   return (
     <Provider store={store}>
       <Router>
+        <Header />
         <Route exact path='/' component={DesignRequest}/>
       </Router>
     </Provider>
--- client/src/components/DesignRequest.js
+++ client/src/components/DesignRequest.js
@@ -1,52 +1,55 @@
 import React from 'react'
-import '../styles/main.css'
+import '../styles/requestForm.css'
+
+import ProjectQueue from './ProjectQueue'
 
 export default props => {
   const handleSubmit = e => {
     e.preventDefault();
   }
   return (
-    <form className='requestForm' onSubmit={handleSubmit}>
-      <label className='formLabel'>Name</label>
-      <input className='formInput' type='text' name='name' />
+    <div className='requestFormContainer'>  
+      <form className='requestForm' onSubmit={handleSubmit}>
+        <label className='formLabel'>Name</label>
+        <input className='formInput' type='text' name='name' />
 
-      <label className='formLabel'>Email</label>
-      <input className='formInput' type='email' name='email' />
+        <label className='formLabel'>Email</label>
+        <input className='formInput' type='email' name='email' />
 
-      <label className='formLabel'>Project Name</label>
-      <input className='formInput' type='text' name='projectName' />
+        <label className='formLabel'>Project Name</label>
+        <input className='formInput' type='text' name='projectName' />
 
-      <label className='formLabel'>Project Type</label>
-      <select className='formInput'>
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="mercedes">Mercedes</option>
-        <option value="audi">Audi</option>
-      </select>
-      <div classname='formCheckbox'>
-        <input className='checkboxInput' type='checkbox' name='printIndividual' /> Print - Individual <br/>
-        <input className='checkboxInput' type='checkbox' name='printMulti' /> Print - Multiple <br/>
-        <input className='checkboxInput' type='checkbox' name='webSocial' /> Web/Social <br/>
-        <input className='checkboxInput' type='checkbox' name='video' /> Video <br/>
-      </div>
+        <label className='formLabel'>Project Type</label>
+        <select className='formInput'>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+        </select>
+        <div classname='formCheckbox'>
+          <input className='checkboxInput' type='checkbox' name='printMulti' /> <span className='checkboxSpan'>Multiple</span> <br/>
+        </div>
 
-      <label id='dueDate' className='formLabel'>Preferred Due Date</label>
-      <input className='formInput' type='date' name='dueDate' />
+        <label id='dueDate' className='formLabel'>Preferred Due Date</label>
+        <input className='formInput' type='date' name='dueDate' />
 
-      <label className='formLabel'>Reference Links</label>
-      <input className='formInput' type='url' name='reference' />
+        <label className='formLabel'>Reference Links</label>
+        <input className='formInput' type='url' name='reference' />
 
-      <label className='formLabel'>Description</label>
-      <textarea 
-        className='formInput'
-        placeholder='All information available on the project' 
-        rows='5' 
-        cols='33' 
-        minLength='1' 
-        maxLength='500'> 
-      </textarea>
+        <label className='formLabel'>Description</label>
+        <textarea 
+          id='description'
+          className='formInput'
+          placeholder='All information available on the project' 
+          rows='5' 
+          cols='33' 
+          minLength='1' 
+          maxLength='500'> 
+        </textarea>
 
-      <button type='submit'>SUBMIT</button>
-    </form>
+        <button className='submit' type='submit'>SUBMIT</button>
+      </form>
+      <ProjectQueue />
+    </div>
   )
 }

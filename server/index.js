220f72f0dd39889919d76deeea4b076f79543e42
Dylan <dmalla@live.com>
2019-10-25 16:51:16 -0700

    Initialized middleware, Created and tested needed routes, setup User route request and validation. Need to set up registration of users.

--- server/index.js
+++ server/index.js
@@ -6,12 +6,22 @@ const app = express()
 // Connect Database
 connectDB()
 
+app.get('/', (req, res) => res.send('API Running'))
+
+// Init Middleware
+app.use(express.urlencoded({extended: false}))
+app.use(express.json({extended: false}))
+app.use(express.static('public'))
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/projects', require('./routes/api/projects'))
 
 const PORT = process.env.PORT || 8000
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
-app.use(express.static('public'))
+
 
 
 app.listen(PORT, () => {
